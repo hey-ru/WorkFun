b180819dfda250673ee279e2682339a1b2820b99
PoorLin <s80532s@gmail.com>
2022-04-25 14:51:00 +0800

    <util> add connectionPool util

---
+++ src/main/java/com/util/ConnectionPool.java
@@ -0,0 +1,28 @@
+package com.util;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class ConnectionPool {
+	
+	public static DataSource getConectPool() {
+	 DataSource ds = null;
+
+		try {
+			Context ctx = new InitialContext();
+			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/CGA101G3");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+		return ds;
+	
+	}
+	
+	
+	
+}
